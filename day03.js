86f49d029a5e8674e40dfab29e1f6d38cd6c7cfb
Manuel Sgobino <msgobino@netcologne.de>
2025-12-03 08:20:13 +0100

    day 03: tidied up solution

--- day03.js
+++ day03.js
@@ -2,26 +2,15 @@
 
 const parseInput = input => input.split('\n').map(l => l.split('').map(Number))
 
-const best = (digits, row) => {
-	let res = ''
-	let pos = -1
-	let l = row.length
+const best = (digits, row, res, pos) => {
 	for (let i = 0; i < digits; i++) {
-		res = res + Math.max(...row.slice(pos + 1, l - digits + 1 + i))
+		res = res + Math.max(...row.slice(pos + 1, row.length - digits + 1 + i))
 		pos += row.slice(pos + 1).indexOf(~~res[res.length - 1]) + 1
-		// console.log({row, digits, res, pos, i, last: res[res.length - 1], ind: row.slice(pos + 1).indexOf(res[res.length - 1])})
 	}
 	return Number(res)
 }
 
-const solve = (digits, input) => {
-	input = parseInput(input)
-	let total = 0
-	for (const nums of input) {
-		total += best(digits, nums)
-	}
-    return total
-}
+const solve = (digits, input) => parseInput(input).reduce((total, row) => total + best(digits, row, '', -1), 0)
 
 const part1 = input => solve(2, input)
 
