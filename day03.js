6400d5cf300356a1700c5b9a16edc530b2618e00
Manuel Sgobino <msgobino@netcologne.de>
2025-12-03 08:10:05 +0100

    day 03: challenge commit part 2, rewrite

--- day03.js
+++ day03.js
@@ -1,31 +1,30 @@
 'use strict'
 
-const { combineConditionally } = require('./utils.js')
+const parseInput = input => input.split('\n').map(l => l.split('').map(Number))
 
-const pairs = tmp => tmp.length == 2
-const tooShort = tmp => tmp.length < 2
-const dozen = tmp => tmp.length == 12
-const notADozen = tmp => tmp.length < 12
+const best = (digits, row) => {
+	let res = ''
+	let pos = -1
+	let l = row.length
+	for (let i = 0; i < digits; i++) {
+		res = res + Math.max(...row.slice(pos + 1, l - digits + 1 + i))
+		pos += row.slice(pos + 1).indexOf(~~res[res.length - 1]) + 1
+		// console.log({row, digits, res, pos, i, last: res[res.length - 1], ind: row.slice(pos + 1).indexOf(res[res.length - 1])})
+	}
+	return Number(res)
+}
 
-const parseInput = input => input.split('\n').map(l => l.split(''))
-
-const solve = (isPart2, input) => {
+const solve = (digits, input) => {
+	input = parseInput(input)
 	let total = 0
 	for (const nums of input) {
-		const combos = isPart2
-			? combineConditionally(nums, dozen, notADozen)
-			: combineConditionally(nums, pairs, tooShort)
-		total += Math.max(...combos.map(c => Number(c.join(''))))
+		total += best(digits, nums)
 	}
     return total
 }
 
-const part1 = input => {
-    return solve(false, parseInput(input))
-}
+const part1 = input => solve(2, input)
 
-const part2 = input => {
-    return solve(true, parseInput(input))
-}
+const part2 = input => solve(12, input)
 
 module.exports = { part1, part2 }
