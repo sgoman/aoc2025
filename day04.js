b9fdf09e3bce615f5d182eefe7c187c44329f8d2
Manuel Sgobino <msgobino@netcologne.de>
2025-12-04 08:24:17 +0100

    day 04: refactored to recursive branchless solver for part 2

--- day04.js
+++ day04.js
@@ -1,8 +1,19 @@
 'use strict'
 
-const { eightWayDeltas, gridCells, getSurrounding, gridToString } = require('./utils.js')
+const { eightWayDeltas, gridCells, getSurrounding } = require('./utils.js')
 
 const parseInput = input => input.trim().split('\n').map(l => l.split(''))
+
+const solve = (grid, total) => {
+	let changed = gridCells(grid)
+		.filter(f => f.value == '@')
+		.reduce((acc, {row, col}) => {
+			const removable = ~~(getSurrounding(grid, row, col, eightWayDeltas).filter(f => f.tile == '@').length < 4)
+			grid[row][col] = '@.'[removable]
+			return acc + removable
+		}, 0)
+	return ((changed == 0) * total) || solve(grid, total + changed)
+}
 
 const part1 = input => {
 	input = parseInput(input)
@@ -11,24 +22,6 @@ const part1 = input => {
 		.reduce((acc, {row, col, value}) => acc + (getSurrounding(input, row, col, eightWayDeltas).filter(f => f.tile == '@').length < 4), 0)
 }
 
-const part2 = input => {
-	input = parseInput(input)
-	let total = 0, papers = gridCells(input).filter(f => f.value == '@'), grabbed
-	do {
-		grabbed = false
-		for (const {row, col, value} of papers) {
-			const touching = getSurrounding(input, row, col, eightWayDeltas).filter(f => f.tile == '@')
-			if (touching.length < 4) {
-				total++
-				grabbed = true
-				input[row][col] = '.'
-			}
-		}
-		console.log(gridToString(input))
-		papers = gridCells(input).filter(f => f.value == '@')
-	} while(papers.length && grabbed)
-
-	return total
-}
+const part2 = input => solve(parseInput(input), 0)
 
 module.exports = { part1, part2 }
