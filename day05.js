126dad994d6f672758a4e10b68ff923b23e69cf9
Manuel Sgobino <msgobino@netcologne.de>
2025-12-05 07:37:31 +0100

    day 05: challenge commit for part 2

--- day05.js
+++ day05.js
@@ -10,17 +10,31 @@ const parseInput = input => {
 	]
 }
 
-const solve = (isPart2, input) => {
-    return input
-}
-
 const part1 = input => {
 	const [ranges, ings] = parseInput(input)
     return ings.reduce((acc, ing) => acc + ranges.some(([l, h]) => ing >= l && ing <= h), 0)
 }
 
 const part2 = input => {
-    return solve(true, parseInput(input))
+	const ranges = parseInput(input)[0]
+	ranges.sort((a, b) => a[0] - b[0])
+	const comp = [ranges[0]]
+	for (const [l, h] of ranges) {
+		let found = 0
+		for (const [i, [s, e]] of [...comp.entries()]) {
+			if (s <= l && l <= e && e <= h) {
+				found = 1
+				comp[i][1] = h
+				break
+			}
+			if (s <= l && l < e && e > h) {
+				found = 1
+				break
+			}
+		}
+		if (found == 0) comp.push([l, h])
+	}
+	return comp.reduce((acc, [l, h]) => acc + h - l + 1, 0)
 }
 
 module.exports = { part1, part2 }
