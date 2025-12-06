c5c1b3ae37a7d58ca7208661bfb8b4ffff20f74a
Manuel Sgobino <manuel.sgobino@gmx.net>
2025-12-06 11:18:53 +0100

    day 06: cleaned up code

--- day06.js
+++ day06.js
@@ -2,14 +2,11 @@
 
 const { transpose } = require('./utils.js')
 
-const parseInput = input => {
-    return input.trim().split('\n').map(l => {
+const part1 = input => {
+    input = input.trim().split('\n').map(l => {
         l = l.trim().replace(/ +/g, ' ').split(' ')
         return [l, l.map(Number)][+(l[0].match(/\d+/) != null)]
     })
-}
-
-const solve = (isPart2, input) => {
     const operations = input.pop()
     return transpose(input)
         .map((vals, i) => [
@@ -19,12 +16,21 @@ const solve = (isPart2, input) => {
         .reduce((acc, cur) => acc + cur, 0)
 }
 
-const part1 = input => {
-    return solve(false, parseInput(input))
-}
-
 const part2 = input => {
-    return solve(true, parseInput(input))
+    input = transpose(input.split('\n').map(l => l.split('')))
+    input = input.map(l => l.map(c => [c, ' '][+(typeof c === 'undefined')])).reverse()
+    return input.reduce(([total, block], line) => {
+        const op = line.pop()
+        const val = Number(line.join(''))
+        block = [[...block, val], block][+(val == 0)]
+        line = [
+            0,
+            block.reduce((a, v) => a + v, 0),
+            block.reduce((a, v) => a * v, 1)
+        ][' +*'.indexOf(op)]
+        block = [ block, [], [] ][' +*'.indexOf(op)]
+        return [total + line, block]
+    }, [0, []])[0]
 }
 
 module.exports = { part1, part2 }
