e02c41e95edf9553c64ba8efe11f2fdf4d683c07
Manuel Sgobino <manuel.sgobino@gmx.net>
2025-12-06 14:07:36 +0100

    day 06: minor refactoring, just compacting

--- day06.js
+++ day06.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { transpose } = require('./utils.js')
+const { transpose, arraySum, arrayProduct } = require('./utils.js')
 
 const part1 = input => {
     input = input.trim().split('\n').map(l => {
@@ -8,29 +8,20 @@ const part1 = input => {
         return [l, l.map(Number)][+(l[0].match(/\d+/) != null)]
     })
     const operations = input.pop()
-    return transpose(input)
-        .map((vals, i) => [
-            vals.reduce((acc, cur) => acc * cur, 1),
-            vals.reduce((acc, cur) => acc + cur, 0)
-        ][+(operations[i] == '+')])
-        .reduce((acc, cur) => acc + cur, 0)
+    return arraySum(transpose(input)
+        .map((vals, i) => [arrayProduct(vals), arraySum(vals)][+(operations[i] == '+')]))
 }
 
-const part2 = input => {
-    input = transpose(input.split('\n').map(l => l.split('')))
-    input = input.map(l => l.map(c => [c, ' '][+(typeof c === 'undefined')])).reverse()
-    return input.reduce(([total, block], line) => {
+const part2 = input => transpose(input.split('\n').map(l => l.split('')))
+    .map(l => l.map(c => [c, ' '][+(typeof c === 'undefined')]))
+    .reverse()
+    .reduce(([total, block], line) => {
         const op = line.pop()
         const val = Number(line.join(''))
         block = [[...block, val], block][+(val == 0)]
-        line = [
-            0,
-            block.reduce((a, v) => a + v, 0),
-            block.reduce((a, v) => a * v, 1)
-        ][' +*'.indexOf(op)]
-        block = [ block, [], [] ][' +*'.indexOf(op)]
+        line = [0, arraySum(block), arrayProduct(block)][' +*'.indexOf(op)]
+        block = [[], block][+(op == ' ')]
         return [total + line, block]
     }, [0, []])[0]
-}
 
 module.exports = { part1, part2 }
