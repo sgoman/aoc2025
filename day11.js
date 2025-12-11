9117a51b09bf363a8dc76e7eb5bdfbc8965b7a48
Manuel Sgobino <manuel.sgobino@gmx.net>
2025-12-11 09:02:53 +0100

    day 11: challenge commit for part 2

--- day11.js
+++ day11.js
@@ -4,22 +4,47 @@
 
 const parseInput = input => input.trim().split('\n').map(l => l.replace(':', '').split(' '))
 
-const solve = (isPart2, input) => {
-    return input
-}
-
-const walk = (wires, target) => {
-    if (target == 'out') return 1
-    return wires
+const walk = (wires, target, path) => {
+    if (target == 'out') {
+        if (path[0] == 'svr') {
+            const ret = (path.includes('fft')) * (path.includes('dac'))
+            console.log(path.join(', '))
+            return ret
+        } else {
+            return 1
+        }
+    }
+    const total = wires
         .filter(f => f[0] == target)[0]
         .slice(1)
-        .reduce((acc, cur) => acc + walk(wires, cur), 0)
+        .reduce((acc, cur) => acc + walk(wires, cur, [...path, cur]), 0)
+    return total
 }
 
-const part1 = input => walk(parseInput(input), 'you')
-
-const part2 = input => {
-    return solve(true, parseInput(input))
+const walkMemo = (wires, target, path, memo) => {
+    const fft = path.includes('fft')
+    const dac = path.includes('dac')
+    const key = `${target},${fft},${dac}`
+    if (memo.has(key)) return memo.get(key)
+    const total = wires
+        .filter(f => f[0] == target)[0]
+        .slice(1)
+        .reduce((acc, cur) => {
+            if (cur == 'out') {
+                if (path[0] == 'svr') {
+                    return acc + (fft && dac)
+                } else {
+                    return acc + 1
+                }
+            }
+            return acc + walkMemo(wires, cur, [...path, cur], memo)
+        }, 0)
+    memo.set(key, total)
+    return total
 }
+
+const part1 = input => walkMemo(parseInput(input), 'you', ['you'], new Map())
+
+const part2 = input => walkMemo(parseInput(input), 'svr', ['svr'], new Map())
 
 module.exports = { part1, part2 }
